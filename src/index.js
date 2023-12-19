776ef7f4e0a3d4084eb44ab729ea16df262de7d0
Eklak Dangaura <azens1995@gmail.com>
2023-12-19 12:37:24 +0545

    feat: add endpoint to generate password

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const log = require('./utils/logger');
+const generatePassword = require('./utils/generatePassword');
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded());
+
 const PORT = 3000;
 
 app.get('/health', (req, res) => {
@@ -10,6 +14,35 @@ app.get('/health', (req, res) => {
   res.json({
     status: 200,
     message,
+  });
+});
+
+app.post('/', (req, res) => {
+  const { size, hasSpecial } = req.body;
+  let errorMessage = '';
+  if (size) {
+    if (typeof size !== 'number') {
+      errorMessage = 'Please provide valid number.\n';
+    }
+    if (typeof size === 'number' && (size < 1 || size > 16)) {
+      errorMessage += 'Size must be between 1 and 16.';
+    }
+  }
+  if (hasSpecial && typeof hasSpecial !== 'boolean') {
+    errorMessage += `hasSpecial must be boolean value.`;
+  }
+  if (errorMessage) {
+    return res.json({ status: 400, error: errorMessage });
+  }
+  const message = 'Successfully generated password.';
+  log.info(message);
+  const password = generatePassword(size, hasSpecial);
+  res.json({
+    status: 200,
+    message,
+    data: {
+      password,
+    },
   });
 });
 
